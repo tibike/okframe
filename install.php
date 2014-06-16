3852a1b1f410622163689beaba46c64a764b5335
tibike <hongzh2002@126.com>
2014-06-16 13:44:12 +0800

    installation file
    tobe invoked in the browser to install/config the framework

---
+++ install.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * okframe: An open-source framework using PHP & jQuery
+ *
+ * For the full copyright and license information, please read
+ * the LICENSE file that was distributed with this source code.
+ */
+
+/**
+ * install okframe from this file
+ *
+ * @category    Core
+ * @package     PACKAGE NAME
+ * @version     1.0.0
+ * @author      Hong Zhang <hongzh2002@126.com>
+ * @copyright   Copyright (c) 2014 - , Hong Zhang
+ * @link        http://www.okframe.com/
+ */
+
+
+/* end of file */
